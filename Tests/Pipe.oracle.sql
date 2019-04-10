9412ffa27a92f83b2bd426e786188234989553a4
Viktor Vshivtsev <wvn@bk.ru>
2019-04-10 14:52:04 +0500

    * SQL

--- Tests/Pipe.oracle.sql
+++ Tests/Pipe.oracle.sql
@@ -3,19 +3,18 @@
 SELECT 
     "ID трубопровода" AS Pipeline_ID
     ,
-    CASE WHEN "Дата изменения">"Дата создания" THEN "Дата изменения" ELSE "Дата создания" END  AS START_TIME
-    ,
-    "Дата удаления" AS END_TIME
-    ,
-	"Предприятие" AS PipeDept_CODE
-	,
-	"Цех" AS PipeShop_CODE
-	,
-	"Месторождение" AS PipeField_CODE
-	,
-	"Площадка" AS PipeSite_Name
-	,
-	"Назначение" AS PipePurpose_CODE
+    CASE WHEN "Дата изменения">"Дата создания" THEN "Дата изменения" ELSE "Дата создания" END  AS START_TIME,
+    "Дата удаления" AS END_TIME,
+
+--************** Administrative bindings via ****************
+--target: PODS.EVENT_GROUP_CROSS_REF ************************
+	"Предприятие" AS PipeOrg_CLASSCODE,
+	"Цех" AS PipeShop_CLASSCODE,
+	"Месторождение" AS PipeField_CLASSCODE,
+	"Площадка" AS PipeSite_NAME,
+
+--target: PODS.LINE
+	"Назначение" AS PipePurpose_CLASSCODE
 	,
     CASE WHEN LENGTH( "Начало трубопровода" || '~' || "Конец трубопровода" ) > 50 
     THEN CONCAT( SUBSTR( "Начало трубопровода" || '~' || "Конец трубопровода", 1, 49 ), '…' )
@@ -27,15 +26,17 @@ SELECT
     ELSE "Начало трубопровода" || '~' || "Конец трубопровода"
     END AS Pipeline_DESCRIPTION
     ,
-    COALESCE( "Назначение", "Тип трубопроводной сети") AS PipelineType_CODE
+    COALESCE( "Назначение", "Тип трубопроводной сети") AS PipelineType_CLASSCODE
     ,
-    "Тип трубопровода" AS PipelineSysType_CODE
+    "Тип трубопровода" AS PipelineSysType_CLASSCODE
     ,
     CASE WHEN "Дата удаления" IS NULL THEN 'Y' ELSE 'N' END   AS Pipeline_CurrIndic
+
 FROM PIPE_TRUBOPROVOD
 ;
 
---Pipeline_To_PipeRoute_Link
+--Pipeline_To_UT_Link
+--Связка трубопровод -> участок трубопровода (UT) OIS Pipe
 SELECT
     "ID трубопровода" AS Pipeline_ID
     ,
@@ -43,40 +44,53 @@ SELECT
     ,
     "Дата удаления" AS END_TIME
     ,
-    "ID участка" AS PipeRoute_ID
+    "ID участка" AS PU_ID
 FROM pipe_uchastok_truboprovod
 ;
 
 --PipeRoutes
 SELECT
-    "ID участка" AS PipeRoute_ID
-    ,
-    CASE WHEN LENGTH( "Начало участка" || '~' || "Конец участка" ) > 254 
+--multitarget fields  ***************************************
+	-- Pipe.UT: поля записи OIS-Pipe "участок трубопровода", соответствующие нескольким целевым таблицам PODS
+	-- Предполагается задавать соответствие с помощью "алиасов"
+    "ID участка"  AS UT_ID,
+    CASE WHEN "Дата удаления" IS NULL THEN 'Y' ELSE 'N' END   AS UT_CurrIndic,
+	CASE WHEN "Дата изменения" > "Дата создания" THEN "Изменено пользователем" ELSE "Создано пользователем" END  AS UT_USER,
+	CASE WHEN "Дата ввода" < "Дата реконструкции участка" THEN  "Дата реконструкции участка" ELSE "Дата ввода" END  AS UT_InstallDate,
+	"Дата создания"  AS UT_CreateDate,
+
+--target: PODS.ROUTE ****************************************
+    CASE WHEN LENGTH("Начало участка")+1+LENGTH("Конец участка") > 254
     THEN CONCAT( SUBSTR( "Начало участка" || '~' || "Конец участка", 1, 253 ), '…' )
     ELSE "Начало участка" || '~' || "Конец участка"
     END  AS PipeRoute_DESCRIPTION
     ,
-    COALESCE("Порядок",0) + "Номер нитки"*32  AS PipeRoute_SequenceNum
-    ,
-    "Назначение" PipeRouteType_CODE
+    COALESCE("Порядок",0) + "Номер нитки"*32  AS PipeRoute_SequenceNum,
+    "Назначение" PipeRouteType_CLASSCODE
 	,
-    CASE WHEN "Дата удаления" IS NULL THEN 'Y' ELSE 'N' END   AS PipeRoute_CurrIndic
-	,
-	CASE WHEN "Дата изменения" > "Дата создания" THEN "Изменено пользователем" ELSE "Создано пользователем" END  AS PipeRoute_USER
-	,
-	"Дата создания"  AS PipeRoute_CreateDate,
-	"Завод изготовитель"  AS PipeSegManuf_CODE,
-	"Материал трубы"  AS PipeSegMaterial_CODE
-	,
-    LTRIM(RTRIM(UPPER(REPLACE("ГОСТ на трубу"||'|'||"ГОСТ на материал",' ')),'|'),'|')  AS PipeSeg_SPECIFICATION
-	,
-    "Класс прочности материала"  AS PipeSegGrade_CODE,
+--target: PODS.PIPE_SEGMENT  ********************************
+	"Завод изготовитель"  AS PipeSegManuf_CLASSCODE,
+	"Материал трубы"  AS PipeSegMaterial_CLASSCODE,
+    LTRIM(RTRIM(UPPER(REPLACE("ГОСТ на трубу"||'|'||"ГОСТ на материал",' ')),'|'),'|')  AS PipeSeg_SPECIFICATION,
+    "Класс прочности материала"  AS PipeSegGrade_CLASSCODE,
     GREATEST(D, 9999.9999)  AS PipeSeg_DIAMETER,
-    GREATEST(S, 99.9999)  AS PipeSegWall_THICKNESS
-	,
-	CASE WHEN "Дата ввода" < "Дата реконструкции участка" THEN  "Дата реконструкции участка" ELSE "Дата ввода" END  AS PipeSeg_InstallDate
-	,
-	CASE WHEN "Дата изменения" > "Дата создания" THEN "Изменено пользователем" ELSE "Создано пользователем" END  AS PipeSeg_USER
+    GREATEST(S, 99.9999)  AS PipeSegWall_THICKNESS,
+
+--target: PODS.EXTERNAL_COATING  ****************************
+	"Завод изготовитель покр"  AS PipeExtCoatManuf_CLASSCODE,
+	"Вид покрытия внешнего"  AS PipeExtCoatKind_CLASSCODE,
+	"Конструкция внеш покрытия"  AS PipeExtCoatKind_GRPCLSCODE,
+	"Условия нанесений внеш покр"  AS PipeExtCoatWhereAppl_CLASSCODE,
+	"ТУ внешнего"  AS PipeExtCoat_SPECIFICATION,
+
+--target: PODS.INTERNAL_COATING  ****************************
+	"Завод изг внутреннего покр"  AS PipeIntCoatManuf_CLASSCODE,
+	"Вид покрытия внутреннего"  AS PipeIntCoatKind_CLASSCODE,
+	"Конструкция внутр покрытия"  AS PipeIntCoatKind_GRPCLSCODE,
+	"Условия нанесения внутр покр"  AS PipeIntCoatWhereAppl_CLASSCODE,
+	"ТУ внутреннего"  AS PipeIntCoat_SPECIFICATION
+
 
 FROM pipe_uchastok_truboprovod
 ;
+
